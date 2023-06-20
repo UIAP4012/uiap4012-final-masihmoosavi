82cc55d69c3d26b2f6d53835a1ce7e876c8cbb65
masihmoosavi <s.masihmoosavi@gmail.com>
2023-06-20 22:52:48 +0430

    Add class MoneyValue for check the equivalence of moneies Add class product Add class pset that is a derivative of set

--- Accounting.cpp
+++ Accounting.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <windows.h>
+#include <set>
 
 #define green HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE) ;SetConsoleTextAttribute(hConsole,10)
 #define red HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE) ;SetConsoleTextAttribute(hConsole,12)
@@ -9,6 +10,64 @@
 
 using namespace std;
 
+//----------------------------------------------------------------------
+class MoneyValue {
+public:
+    double IRR = 1 ; // value of USD/IRR
+    double EUR = 1 ; // value of USD/EUR
+    void update_price () {
+        yelow;
+        cout << "Enter the equivalent value" << endl ;
+        cout << "1 USD = ? IRR" << endl ;
+        double r ; // 1 USD = r IRR
+        cin >> r ;
+        IRR = r ;
+        cout << "Enter the equivalent value" << endl ;
+        cout << "1 USD = ? EUR" << endl ;
+        double e ; // 1 USD = e EUR
+        cin >> e ;
+        EUR = e ;
+    }
+};
+//-----------------------------------------------------------------------
+class product {
+public:
+    long long ID ;
+    string Name ;
+    int Number ;
+    double Price ;
+    void print ( MoneyValue a ) {
+        cout << "ID : " << ID << endl ;
+        cout << "Name : " << Name << endl ;
+        cout << "Number of available : " << Number << endl ;
+        cout << "Price : " << Price << " USD = " << Price*a.EUR
+             << " EUR = " << Price*a.IRR << " IRR" << endl << endl ;
+    }
+    bool operator < (product b) {
+        if ( this->Name < b.Name )
+            return true ;
+        if ( this->Name > b.Name )
+            return false ;
+        if ( this->ID < b.ID )
+            return true ;
+        return false;
+    }
+
+};
+//------------------------------------------------------------------------
+class pset : public set<product> {
+public:
+    // search by name
+    bool search (string name) {
+        for (const auto &i : (*this) )
+            if ( i.Name == name )
+                return  true ;
+        return false ;
+    }
+};
+//------------------------------------------------------------------------
+//||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//------------------------------------------------------------------------
 
 
 int main()
