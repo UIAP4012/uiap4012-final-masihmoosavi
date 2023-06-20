0cf94fdd8eff10bab7ee6a2d8b395edada2522d5
masihmoosavi <s.masihmoosavi@gmail.com>
2023-06-20 19:42:00 +0430

    Add colors;

--- Accounting.cpp
+++ Accounting.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+#include <windows.h>
+
+#define green HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE) ;SetConsoleTextAttribute(hConsole,10)
+#define red HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE) ;SetConsoleTextAttribute(hConsole,12)
+#define blue HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE) ;SetConsoleTextAttribute(hConsole,11)
+#define white HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE) ;SetConsoleTextAttribute(hConsole,7)
+#define yelow HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE) ;SetConsoleTextAttribute(hConsole,6)
 
 using namespace std;
 
+
+
 int main()
 {
-    cout << "Hello World!" << endl;
     return 0;
 }
